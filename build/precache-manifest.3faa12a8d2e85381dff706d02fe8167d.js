ae6d595a165c0fff33684bf7967562e0930e1a83
KariKorpinen <karikkorpinen@gmail.com>
2020-02-06 11:25:37 +0200

    frontBack2

---
+++ build/precache-manifest.3faa12a8d2e85381dff706d02fe8167d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20af1ef61e15e7217cf045f0faf20e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d81e086e149bebc8ef65",
+    "url": "/static/css/main.252dd0d5.chunk.css"
+  },
+  {
+    "revision": "68f46495350dcc5b40d9",
+    "url": "/static/js/2.72cafd1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72cafd1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d81e086e149bebc8ef65",
+    "url": "/static/js/main.1360fa49.chunk.js"
+  },
+  {
+    "revision": "e2d781b1a5b86d3e13a3",
+    "url": "/static/js/runtime-main.61263b4b.js"
+  }
+]);
